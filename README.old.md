1ad81f1725445e9c92cb10ad5ed37e9469123a50
Ilija Pejanović <ikadvm96@gmail.com>
2023-08-22 21:16:31 +0200

    initial commit, project setup

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# news-app
+This web app is a news aggregator that fetches news articles using APIs and displays them in an attractive format.
+
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import Home from './components/Home';
+
+export const App: React.FC = () => {
+  return (
+    <div className="App">
+      {
+        <div>Initial commit</div>
+      }
+    </div>
+  );
+};
